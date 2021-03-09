4127f59371af8a467092c4e25178c58ee6da8d22
Tshering <engineertshering@gmail.com>
2021-03-09 14:11:37 +0545

    Hello world paragraph is added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import SearchBox from './component/searchbox/search.component.jsx';
 import './App.css';
 
-class App extends React.Component{
-                 render(){
-                          return(
-                                 <div>
-                                      <h1>Welcome To Shop</h1>
-                                      <SearchBox />
-                                 </div>
-                          )
-                 }
+function App(){
+                  return(
+                         <div>
+                              Hello World
+                         </div>
+                  )
 }
-
 export default App;
