da0307361526299e17965eb9be57a4ff188978cd
Tshering <engineertshering@gmail.com>
2021-03-10 08:54:42 +0545

    changes into app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import HomePage from './homepage.component.jsx';
 import './App.css';
 
 function App(){
                   return(
                          <div>
-                              Hello World
+                              
+                              <HomePage />
                          </div>
                   )
 }
