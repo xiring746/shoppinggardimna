4a4ce5decbb06cc9e6f0a10fa63c18846d563092
Tshering <maneyonjan123@gmail.com>
2021-03-23 11:23:02 +0545

    storing user data in firebase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage//homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import Header from './components/header/header.component.jsx';
-import { auth } from './firebase/firebase.utils.js'; 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js'; 
 import './App.css';
 
 
@@ -23,9 +23,10 @@ class App extends React.Component{
      }
      unsubscribeFromAuth = null
      componentDidMount(){
-                this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-                     this.setState({ currentUser: user });
-                     console.log(user);
+                this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+                     createUserProfileDocument(user);
+                    //  this.setState({ currentUser: user });
+                    //  console.log(user);
                 });
      }
      componentWillUnmount(){
