9b865e2bfca86285407acaa9c42136e0c2a87844
Tshering <maneyonjan123@gmail.com>
2021-06-02 21:21:17 +0545

    created cart-item component and connected our cart dropdown with cartItems reducer

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -8,8 +8,9 @@ const PreviewCollection = ({title, items}) => (
                          <h1 className="title">{title.toUpperCase()}</h1>
                          <div className="preview">
                               {
-                                items.filter((item, idx)=> idx <4 ).map(({ id, ...otherPropsItem}) => (
-                                       <CollectionItem key={id} {...otherPropsItem} />
+                                items.filter((item, idx)=> idx <4 )
+                                .map(( item ) => (
+                                       <CollectionItem key={item.id} item={item} />
                                 ))
                               }
                          </div>
